eb51f519032ecced6fa9dd346d05ea132c41098b
Christian Daryl Curay <80902959+christiancuray@users.noreply.github.com>
2025-04-04 20:47:50 -0400

    initial commit

---
+++ Desktop/Projects/reactt-portfolio/portfolio/src/components/About.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="flex flex-col relative items-center w-1/2 h-screen overflow-y-auto scroll-smooth">
+      <p>hello there testing</p>
+    </div>
+  );
+}
